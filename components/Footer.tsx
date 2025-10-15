9faee70b36a0d76c0240246ea190d82ca5956032
michaelwnau <michaelwnau@gmail.com>
2025-10-15 11:18:38 -0500

    update Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,16 +16,14 @@ export default function Footer() {
 
   return (
     <footer
-      className={`${alfa.className} fixed bottom-4 left-4 right-4 md:left-[565px] font-bold text-[24pt] text-black/100 z-40`}
+      className={`${alfa.className} fixed bottom-0 left-0 right-0 p-6 md:p-8 lg:p-10 font-bold text-black/100 z-40 bg-[#f9f908]`}
     >
-      <div className="grid grid-cols-3 gap-4 items-center">
-        <div className="text-left">
-          {/* Empty left column */}
-        </div>
+      {/* Mobile: stacked layout */}
+      <div className="flex flex-col gap-2 items-center sm:hidden text-[16pt]">
         <div className="text-center">
           <Link href="/contact" className="relative group inline-block">
             <span className="relative z-10">contact</span>
-            <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[395px] h-[100px] opacity-0 group-hover:opacity-100 transition-opacity z-0">
+            <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[200px] h-[50px] opacity-0 group-hover:opacity-100 transition-opacity z-0">
               <Image
                 src={imgContactOnHover}
                 alt="contact hover"
@@ -35,7 +33,30 @@ export default function Footer() {
             </div>
           </Link>
         </div>
-        <div className="text-right">
+        <div className="text-center">
+          © bornless labs, {currentYear}
+        </div>
+      </div>
+
+      {/* Tablet and Desktop: side-by-side layout with flexbox */}
+      <div className="hidden sm:flex items-baseline justify-between text-[18pt] sm:text-[20pt] lg:text-[24pt]">
+        <div className="flex-1">
+          {/* Empty space for balance */}
+        </div>
+        <div className="flex-shrink-0">
+          <Link href="/contact" className="relative group inline-block">
+            <span className="relative z-10">contact</span>
+            <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 w-[250px] sm:w-[300px] lg:w-[395px] h-[60px] sm:h-[80px] lg:h-[100px] opacity-0 group-hover:opacity-100 transition-opacity z-0">
+              <Image
+                src={imgContactOnHover}
+                alt="contact hover"
+                fill
+                style={{ objectFit: 'contain' }}
+              />
+            </div>
+          </Link>
+        </div>
+        <div className="flex-1 text-right">
           © bornless labs, {currentYear}
         </div>
       </div>
